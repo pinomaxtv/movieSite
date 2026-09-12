8ce6672dd7bbeb03858417b4c9d6faa1672c8f9e
pinomaxtv <streamixph@gmail.com>
2026-09-12 21:15:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const title = document.querySelector("app-title");
 const searchBox = document.querySelector(".search-box");
 const icon = document.querySelector(".search-icon");
 
-icon.addEventListener('click', function() {
-  alert("GUMAGANA ANG PINDOT BOSS!");
+icon.addEventListener('click', 
+function() {
+ searchBox.style.display = "none";    // Itago ang search box
+    logo.style.display = "block";        // Ibalik ang logo
+    title.style.display = "block";       // Ibalik ang title
+    icon.src = "https://cdn-icons-png.flaticon.com/512/622/622669.png"; // Ibalik sa Magnifying glass
+  }
+  else {
+    searchBox.style.display = "block";   // Ilabas ang search box!
+    logo.style.display = "none";         // Itago ang logo!
+    title.style.display = "none";        // Itago ang title!
+    icon.src = "https://cdn-icons-png.flaticon.com/512/2961/2961937.png"; // Palitan ng icon na 'X'!
+}
 });
