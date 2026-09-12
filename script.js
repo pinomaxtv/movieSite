c04c906c3e5adedb6e2d360a845e16fb3022f849
pinomaxtv <streamixph@gmail.com>
2026-09-12 20:56:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const icon = document.querySelector('.search-icon');
+const logo = document.querySelector(".logoleft");
+const title = document.querySelector("app-title");
+const searchBox = document.querySelector(".search-box");
+const icon = document.querySelector(".search-icon");
 
 icon.addEventListener('click', function() {
   alert("GUMAGANA ANG PINDOT BOSS!");
